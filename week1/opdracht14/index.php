c758965ece755bd7a3a77233b5322cea4eae120c
Joeri Roijenga <joeriroijenga@macbook-pro-van-joeri.local>
2018-09-12 22:21:58 +0200

    Opdracht 14

--- week1/opdracht14/index.php
+++ week1/opdracht14/index.php
@@ -1,11 +1,39 @@
+<?php
+spl_autoload_register(function ($class_name) {
+    include "./classes/" . $class_name . ".php";
+});
+
+$function = new functions();
+$fruitChoice = $function->getPostValue();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Opdracht 1</title>
+    <title>Opdracht 14</title>
 </head>
 <body>
-    <h1>Opdracht 1:</h1>
+    <h1>Opdracht 14:</h1>
 
+    <form action="#" method="post">
+        <select name="fruit-choice">
+            <option value="">Maak en keuze</option>
+            <option value="appel">Appel</option>
+            <option value="banaan">Banaan</option>
+            <option value="peer">Peer</option>
+        </select>
+        <input type="submit" value="Toon Keuze" />
+    </form>
+    <br />
+    <?php
+    if ($fruitChoice) {
+        ?>
+        <form action="#" method="post">
+            <label>Uw favoriete fruitsoort is een <?php echo $fruitChoice; ?>!</label><br />
+            <input type="submit" value="Verwijder keuze" />
+        </form>
+        <?php
+    }
+    ?>
 </body>
 </html>
