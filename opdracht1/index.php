5df15da709b7603bb2cfeca0c6803e7d617fcd54
Joeri Roijenga <joeriroijenga@macbook-pro-van-joeri.local>
2018-09-13 12:44:47 +0200

    Opdracht 1.

--- opdracht1/index.php
+++ opdracht1/index.php
@@ -1,1 +1,18 @@
-Hallo wereld!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Opdracht 1</title>
+</head>
+<body>
+    <h1>Opdracht 1:</h1>
+    <?php
+    $address = "Van Doorenveste, Zernikeplein 11, 9747 AS, Groningen";
+    list($name, $address, $postalCode, $place) = explode(", ", $address);
+    ?>
+    <label><b>Naam van het gebouw:</b> <?php echo $name; ?></label><br />
+    <label><b>Adres:</b> <?php echo $address; ?></label><br />
+    <label><b>Postcode:</b> <?php echo $postalCode; ?></label><br />
+    <label><b></b>Plaats:</b> <?php echo $place; ?></label><br />
+</body>
+</html>
